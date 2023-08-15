f5d47e04c5892a35068fb1f5970852a76e992d0d
puneeth-y-s <yspuneeth1994@gmail.com>
2023-08-15 16:57:00 +0530

    initial commit

---
+++ drfecommerce/tests/test_example.py
@@ -0,0 +1,14 @@
+def test_example():
+    assert 1 == 1
+
+def test_mul():
+    assert 2 * 3 == 6
+
+def test_div():
+    assert 6 / 3 == 2
+
+def test_mod():
+    assert 8 % 2 == 0
+
+def test_equality():
+    assert (1, 2, 3) == (1, 2, 3)
